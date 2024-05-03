dfdf2e46466aff5cf4c2bc87bd7f2c7f856d0b5f
estacioFilho <estacioqueirozfilho@gmail.com>
2024-05-02 22:37:48 -0300

    Escondendo formulário

--- script-crud.js
+++ script-crud.js
@@ -14,20 +14,24 @@ formulario.addEventListener('submit', (event) => {
     const tarefa = {
         descricao: textArea.value
     }
+
     listaTarefas.push(tarefa);
+    const elementeTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementeTarefa);
     localStorage.setItem('tarefas', JSON.stringify(listaTarefas));
+    textArea.value = '';
+    formulario.classList.add('hidden');
 })
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li');
     li.classList.add('app__section-task-list-item');
     const svg = document.createElement('svg');
-    svg.inerHTML = ` <svg>
+    svg.innerHTML = `
     <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
         <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-    </svg>
-</svg>`;
+    </svg>`;
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('app__section-task-list-item-description');
     paragrafo.textContent = tarefa.descricao;
